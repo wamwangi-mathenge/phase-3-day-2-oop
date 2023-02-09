6bebe954ca258ed1bd29d79d22ba71a00b74fe1e
Brian <wamwangimathenge@gmail.com>
2023-02-09 12:25:07 +0300

    Pry gem

--- run.rb
+++ run.rb
@@ -2,3 +2,9 @@
 # Properties/attributes: name, model, year, color, price, fuel
 # Methods/behavior => functions that give our objects behavior eg. accelerate, brake, stolen
 
+require 'pry'
+
+class Cars
+end
+
+binding.pry
