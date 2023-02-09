11be47624abf0b3c9c62fef692787e089eb47708
Brian <wamwangimathenge@gmail.com>
2023-02-09 12:52:33 +0300

    Getter and setter methods

--- run.rb
+++ run.rb
@@ -6,14 +6,39 @@ require 'pry'
 
 class Cars
 
-    def accelerate
-        puts "Vrooom!!!"
+
+    # Setter method for name instance variable
+    # Can be replaced in future with attr_writer/attr_accessor
+    def name=(name)
+        @name = name
+    end
+
+    # Setter method for model instance variable
+    def model=(model)
+        @model = model
+    end
+
+    # Getter method for name model instance variable
+    # Can be replaced in future with attr_reader
+    def name
+        @name
+    end
+
+    # Getter method for model instance variable
+    def model
+        @model
+    end
+
+    def accelerate(name)
+        puts "Vrooom!!! #{name}"
     end
 
 end
 
-brianscar = Cars.new
+brianscar = Cars.new # Instance method
 
-brianscar.accelerate
+brianscar.accelerate(brianscar)
+
+
 
 binding.pry
